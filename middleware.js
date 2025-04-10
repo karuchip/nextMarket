84eda5b19ff914d9054808f8e605862304f31a2b
Hikaru Hatakeyama <hkr.12karu@gmail.com>
2025-04-10 17:56:42 +0900

    [add]フロントサイドを追加

--- middleware.js
+++ middleware.js
@@ -2,8 +2,7 @@ import { jwtVerify } from "jose";
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-  const token = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImR1bW15QGdtYWlsLmNvbSIsImV4cCI6MTc0MzU4MTU0N30.0DIiCAenW_vjCAYYnhyWiXoZFXYy5sb_R2gswW7T6Bw"
-  // await request.headers.get("Authorization")?.split(" ")[1]
+  const token = await request.headers.get("Authorization")?.split(" ")[1];
 
   if(!token) {
     return NextResponse.json({message: "トークンがありません"})
