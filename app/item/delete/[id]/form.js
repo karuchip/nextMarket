b28bc9a9e1fcca682e914ef2059b98e11f20f3fd
Hikaru Hatakeyama <hkr.12karu@gmail.com>
2025-04-11 16:56:49 +0900

    [Add] updateとdeleteページについて、サーバーコンポーネントとAction機能の導入

---
+++ app/item/delete/[id]/form.js
@@ -0,0 +1,55 @@
+"use client"
+import { useState, useEffect } from "react"
+import {useRouter} from "next/navigation"
+import Image from "next/image"
+import useAuth from "@/app/utils/useAuth"
+
+const DeleteItem = (props) => {
+
+  const router = useRouter();
+  const loginUserEmail = useAuth()
+
+
+
+  const handleSubmit = async(e) => {
+     e.preventDefault();
+
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/delete/${props.params.id}`, {
+        method: "DELETE",
+        headers: {
+          "Accept": "application.json",
+          "COntent-Type": "application/json",
+          "Authorization": `Bearer ${localStorage.getItem("token")}`
+        },
+        body: JSON.stringify({
+          email: loginUserEmail
+        })
+      })
+      const jsonData = await response.json()
+      alert(jsonData.message)
+      router.push("/")
+    } catch(err) {
+      alert("アイテム削除失敗")
+    }
+  }
+  if (loginUserEmail === props.singleItem.email) {
+    return(
+      <div>
+
+      <form onSubmit={handleSubmit}>
+        <h2>{props.singleItem.title}</h2>
+        <Image src={props.singleItem.image} width={750} height={500} alt="Item-Image" priority />
+        <h3>¥{props.singleItem.price}</h3>
+        <p>{props.singleItem.description}</p>
+        <button>削除</button>
+      </form>
+
+      </div>
+    )
+  } else{
+    return<h1>権限がありません</h1>
+  }
+}
+
+export default DeleteItem
--- app/item/readsingle/[id]/page.js
+++ app/item/readsingle/[id]/page.js
@@ -2,7 +2,7 @@
 import Image from "next/image"
 import Link from "next/link"
 
-const getSingleItem = async(id) => {
+export const getSingleItem = async(id) => {
   console.log(id);
   const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/readsingle/${id}`)
   const jsonData = await response.json();
